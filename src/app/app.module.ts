0b4c50c56879b8c1cb84c8a871e3ebe44f0678f9
Aleff Trajano <aleff_diniz@hotmail.com>
2019-02-14 14:55:13 -0300

    criando o mudulo para tarefas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { TarefaModule } from './tarefa';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    TarefaModule,
     
   ],
   providers: [],
